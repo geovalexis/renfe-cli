eff8e5b3d05cd4827a03aa3b426573cca5492401
Geovanny Risco <56429448+geovalexis@users.noreply.github.com>
2023-10-22 15:53:36 +0000

    add fastapi boilerplate

--- setup.py
+++ setup.py
@@ -2,53 +2,57 @@ from setuptools import find_packages, setup
 
 try:
     from pypandoc import convert_file
-    read_md = lambda f: convert_file(f, 'rst')
+
+    read_md = lambda f: convert_file(f, "rst")
 except ImportError:
     print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
+    read_md = lambda f: open(f, "r").read()
 
 setup(
-    name='renfe-cli',
-    version='3.3.0',
-    description='Get faster RENFE Spanish Trains timetables in your terminal',
-    long_description=read_md('README.md'),
-    keywords='Get faster RENFE Spanish Trains timetables terminal',
-    author='Gerard Castillo',
-    author_email='gerardcl@gmail.com',
-    url='https://github.com/gerardcl/renfe-cli',
-    license='BSD',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    py_modules=['renfe-cli'],
+    name="renfe-cli",
+    version="3.4.0",
+    description="Get faster RENFE Spanish Trains timetables in your terminal",
+    long_description=read_md("README.md"),
+    keywords="Get faster RENFE Spanish Trains timetables terminal",
+    author="Gerard Castillo",
+    author_email="gerardcl@gmail.com",
+    url="https://github.com/gerardcl/renfe-cli",
+    license="BSD",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=["renfe-cli"],
     include_package_data=True,
     install_requires=[
-        'setuptools>=60.9.3',
-        'beautifulsoup4==4.10.0',
-        'html5lib==1.1',
-        'selenium==4.12.0',
-        'webdriver-manager==4.0.0',
-        'requests==2.31.0',
-        'colorama>=0.4.6,<1.0',
+        "setuptools>=60.9.3",
+        "beautifulsoup4==4.10.0",
+        "html5lib==1.1",
+        "selenium==4.12.0",
+        "webdriver-manager==4.0.0",
+        "requests==2.31.0",
+        "colorama>=0.4.6,<1.0",
+        "fastapi>=0.104.0,<1.0",
+        "uvicorn>=0.23.0,<1.0",
     ],
     entry_points="""
         [console_scripts]
         renfe-cli = renfe.cli:main
+        renfe-api = renfe.api:main
         """,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: MacOS',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Topic :: Utilities',
-        'Topic :: Terminals',
-        'Topic :: Text Processing :: Markup :: HTML',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+        "Topic :: Terminals",
+        "Topic :: Text Processing :: Markup :: HTML",
     ],
-    tests_require=['pytest'],
-    test_suite = 'pytest',
+    tests_require=["pytest"],
+    test_suite="pytest",
 )
